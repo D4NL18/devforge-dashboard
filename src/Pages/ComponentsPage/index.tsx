78defc4d7d3c8a41b081dc0b1262cb14a256f818
Alexandre Magno Pinho Correia de Oliveira <xandemagnocorreia@gmail.com>
2025-09-08 17:35:11 -0300

    feat/table_component adjusting table colors and borders

--- src/Pages/ComponentsPage/index.tsx
+++ src/Pages/ComponentsPage/index.tsx
@@ -9,27 +9,26 @@ const ComponentsPage = () => {
   return (
     <div className={styles.container}>
       <Navbar />
-        <h2>📦 Componentes</h2>
-
-        <section>
-          <h3>Tabela de Usuários</h3>
-          <PaginatedTable
-            columns={[
-              { key: "id", label: "ID" },
-              { key: "name", label: "Nome" },
-              { key: "email", label: "Email" }
-            ]}
-            data={[
-              { id: 1, name: "Alice", email: "alice@email.com" },
-              { id: 2, name: "Bob", email: "bob@email.com" },
-              { id: 3, name: "Carol", email: "carol@email.com" },
-              { id: 4, name: "Dan", email: "dan@email.com" },
-              { id: 5, name: "Eve", email: "eve@email.com" },
-              { id: 6, name: "Frank", email: "frank@email.com" }
-            ]}
-            rowsPerPage={4}
-          />
-        </section>
+      <h2>📦 Componentes</h2>
+      <section>
+        <h3>Tabela de Usuários</h3>
+        <PaginatedTable
+          columns={[
+            { key: "id", label: "ID" },
+            { key: "name", label: "Nome" },
+            { key: "email", label: "Email" }
+          ]}
+          data={[
+            { id: 1, name: "Alice", email: "alice@email.com" },
+            { id: 2, name: "Bob", email: "bob@email.com" },
+            { id: 3, name: "Carol", email: "carol@email.com" },
+            { id: 4, name: "Dan", email: "dan@email.com" },
+            { id: 5, name: "Eve", email: "eve@email.com" },
+            { id: 6, name: "Frank", email: "frank@email.com" }
+          ]}
+          rowsPerPage={4}
+        />
+      </section>
       <Footer />
     </div>
   );
