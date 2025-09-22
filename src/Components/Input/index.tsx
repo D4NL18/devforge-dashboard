4142658a707f2dfd61ef35d120d9cba4616f7175
Daniel Marinho <danielmarinho8@hotmail.com>
2025-09-22 19:14:30 -0300

    input adjustments

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.scss";
 import { FaRegCalendarAlt } from "react-icons/fa";
 
 type InputProps = {
-  type: "text" | "date" | "number" | string;
+  type: string;
   placeholder?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -12,18 +12,12 @@ type InputProps = {
 
 const Input = ({ type, placeholder, value, onChange, required }: InputProps) => {
   if (type === "date") {
-    // Cria uma referência para o elemento input da data.
     const dateInputRef = useRef<HTMLInputElement>(null);
 
-    // Esta função é chamada quando o contêiner (div) é clicado.
     const handleWrapperClick = () => {
-      // Se a referência estiver conectada ao input, chama o método showPicker().
-      // Este método abre o seletor de data nativo do navegador.
       dateInputRef.current?.showPicker();
     };
 
-    // Uma classe é adicionada ao contêiner quando não há valor,
-    // permitindo ao CSS mostrar o placeholder personalizado.
     const wrapperClassName = value
       ? styles.dateInputWrapper
       : `${styles.dateInputWrapper} ${styles.noValue}`;
@@ -37,20 +31,13 @@ const Input = ({ type, placeholder, value, onChange, required }: InputProps) => 
           value={value}
           onChange={onChange}
           required={required}
-          // Impede que o usuário digite no campo de data.
           onKeyDown={(e) => e.preventDefault()}
         />
-        {/* Este span atua como nosso placeholder personalizado */}
         <span className={styles.placeholderText}>{placeholder}</span>
-        {/* Ícone importado da biblioteca react-icons */}
         <FaRegCalendarAlt className={styles.calendarIcon} />
       </div>
     );
   }
-
-  /**
-   * Para todos os outros tipos de input, renderiza um elemento input padrão.
-   */
   return (
     <input
       className={styles.input}
